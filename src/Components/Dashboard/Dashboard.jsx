e6d1ddebc97d1f19b1a9bee3fbe8d1e2e6ee5186
Mustazir Billah <billah15-5193@diu.edu.bd>
2025-01-29 17:03:36 +0600

    final version

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,50 +1,50 @@
-// import { Outlet } from "react-router-dom";
-// import StudentNav from "./Student/StudentNav";
-// import TuitorNav from "./Tuitor/TuitorNav";
-// import AdminNav from "./Admin/AdminNav";
+import { Outlet } from "react-router-dom";
+import StudentNav from "./Student/StudentNav";
+import TuitorNav from "./Tuitor/TuitorNav";
+import AdminNav from "./Admin/AdminNav";
 
-// import useRole from "../Hooks/useRole";
+import useRole from "../Hooks/useRole";
 
 
 
-// const Dashboard = () => {
-//     const [user, isLoading] = useRole()
+const Dashboard = () => {
+    const [user, isLoading] = useRole()
     
-//     if(isLoading){
-//         return <div className='flex items-center justify-center w-full pt-2 h-screen'>
-//             <span className="loading loading-bars loading-lg"></span>
-//         </div>
-//     }
-//     if (user.role == 'Admin') {
-//         return (
-//             <div className='font-lato md:flex'>
-//                 <AdminNav></AdminNav>
-//                 <Outlet></Outlet>
+    if(isLoading){
+        return <div className='flex items-center justify-center w-full pt-2 h-screen'>
+            <span className="loading loading-bars loading-lg"></span>
+        </div>
+    }
+    if (user.role == 'Admin') {
+        return (
+            <div className='font-lato md:flex'>
+                <AdminNav></AdminNav>
+                <Outlet></Outlet>
 
-//             </div>
-//         );
-//     }
-//     if (user.role == 'Student') {
-//         return (
-//             <div className='font-lato md:flex'>
-//                 <StudentNav></StudentNav>
-//                 <Outlet></Outlet>
+            </div>
+        );
+    }
+    if (user.role == 'Student') {
+        return (
+            <div className='font-lato md:flex'>
+                <StudentNav></StudentNav>
+                <Outlet></Outlet>
 
-//             </div>
-//         );
-//     }
-//     if (user.role == 'Tuitor') {
-//         return (
-//             <div className='font-lato md:flex'>
-//                 {/* <StudentNav></StudentNav> */}
-//                 <TuitorNav></TuitorNav>
-//                 {/* <AdminNav></AdminNav> */}
-//                 <Outlet></Outlet>
+            </div>
+        );
+    }
+    if (user.role == 'Tuitor') {
+        return (
+            <div className='font-lato md:flex'>
+                {/* <StudentNav></StudentNav> */}
+                <TuitorNav></TuitorNav>
+                {/* <AdminNav></AdminNav> */}
+                <Outlet></Outlet>
 
-//             </div>
-//         );
-//     }
+            </div>
+        );
+    }
 
-// };
+};
 
-// export default Dashboard;
+export default Dashboard;
