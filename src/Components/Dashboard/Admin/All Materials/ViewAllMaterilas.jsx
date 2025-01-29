1a1d32d09a5d5206913b8f76878c8b65435310fa
Mustazir Billah <billah15-5193@diu.edu.bd>
2025-01-29 16:56:55 +0600

    admin role

--- src/Components/Dashboard/Admin/All Materials/ViewAllMaterilas.jsx
+++ src/Components/Dashboard/Admin/All Materials/ViewAllMaterilas.jsx
@@ -1,96 +1,96 @@
-// import React from 'react';
-// import useMaterials from '../../../Hooks/useMaterials';
-// import axios from 'axios';
-// import Swal from 'sweetalert2';
+import React from 'react';
+import useMaterials from '../../../Hooks/useMaterials';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
-// const ViewAllMaterilas = () => {
-//     const [materials, isLoading, refetch] = useMaterials();
-//     console.log(materials)
-//     if (isLoading) {
-//         return <div className='flex items-center justify-center w-full pt-2 h-screen'>
-//             <span className="loading loading-bars loading-lg"></span>
-//         </div>
-//     }
-//     const handelDelete = id => {
-//         Swal.fire({
-//             title: "Are you Sure ?",
-//             text: "Please Think once before Deleting",
-//             icon: "warning",
-//             showCancelButton: true,
-//             cancelButtonText: "Wait",
-//             confirmButtonColor: "#4a7fce",
+const ViewAllMaterilas = () => {
+    const [materials, isLoading, refetch] = useMaterials();
+    console.log(materials)
+    if (isLoading) {
+        return <div className='flex items-center justify-center w-full pt-2 h-screen'>
+            <span className="loading loading-bars loading-lg"></span>
+        </div>
+    }
+    const handelDelete = id => {
+        Swal.fire({
+            title: "Are you Sure ?",
+            text: "Please Think once before Deleting",
+            icon: "warning",
+            showCancelButton: true,
+            cancelButtonText: "Wait",
+            confirmButtonColor: "#4a7fce",
 
-//             cancelButtonColor: "#262d53",
-//             confirmButtonText: "Yes, Delete!"
-//         }).then(async (result) => {
-//             if (result.isConfirmed) {
+            cancelButtonColor: "#262d53",
+            confirmButtonText: "Yes, Delete!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
 
                 
-//                 axios.delete(`https://skillpath-bay.vercel.app/materials/${id}`)
-//                     .then(res => {
-//                         if (res.data.acknowledged == true) {
-//                             Swal.fire({
-//                                 title: "Delete Done!",
-//                                 text: "Your file has been Deleted",
-//                                 icon: "success"
-//                             });
-//                             refetch()
-//                         }
+                axios.delete(`https://skillpath-bay.vercel.app/materials/${id}`)
+                    .then(res => {
+                        if (res.data.acknowledged == true) {
+                            Swal.fire({
+                                title: "Delete Done!",
+                                text: "Your file has been Deleted",
+                                icon: "success"
+                            });
+                            refetch()
+                        }
                         
-//                     })
+                    })
                     
 
-//             }
-//         });
-//     }
-//     return (
-//         <div className="p-6 bg-gray-200 space-y-4 w-full">
-//             <div className="p-6 sm:p-12 bg-gray-50 text-gray-800 w-full">
-//                 <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+            }
+        });
+    }
+    return (
+        <div className="p-6 bg-gray-200 space-y-4 w-full">
+            <div className="p-6 sm:p-12 bg-gray-50 text-gray-800 w-full">
+                <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
 
-//                     <div className="flex flex-col">
-//                         <h4 className="text-2xl font-semibold font-Noto text-center md:text-left">All Materials</h4>
-//                         <p className="text-gray-600"></p>
-//                     </div>
-//                 </div>
+                    <div className="flex flex-col">
+                        <h4 className="text-2xl font-semibold font-Noto text-center md:text-left">All Materials</h4>
+                        <p className="text-gray-600"></p>
+                    </div>
+                </div>
 
-//             </div>
-//             <div className="p-6 sm:p-12 bg-gray-50 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-//                 {materials.map((course) => (
-//                     <div
-//                         key={course._id}
-//                         className="bg-white rounded-lg shadow-md overflow-hidden"
-//                     >
-//                         <img
-//                             src={course.photo}
-//                             alt={course.title}
-//                             className="w-full h-48 object-cover"
-//                         />
-//                         <div className="p-4">
-//                             <h2 className="text-xl font-semibold mb-2">{course.title}</h2>
-//                             <p className="text-gray-600 mb-4">{course.description}</p>
-//                             <p className="text-gray-700 font-medium mb-2">
-//                                 Sessions Title: <span className='text-color2'>{course.sessionTitle}</span>
-//                             </p>
-//                             <p className="text-gray-700 font-medium mb-2">
-//                                 Drive Link: {course.driveLink}
-//                             </p>
-//                             <p className="text-gray-500 text-sm">
-//                                 Instructor: {course.tuitorUsername} (
-//                                 {course.tuitorEmail})
-//                             </p>
-//                             <div className="divider"></div>
-//                             <div className='flex justify-between '>
+            </div>
+            <div className="p-6 sm:p-12 bg-gray-50 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {materials.map((course) => (
+                    <div
+                        key={course._id}
+                        className="bg-white rounded-lg shadow-md overflow-hidden"
+                    >
+                        <img
+                            src={course.photo}
+                            alt={course.title}
+                            className="w-full h-48 object-cover"
+                        />
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold mb-2">{course.title}</h2>
+                            <p className="text-gray-600 mb-4">{course.description}</p>
+                            <p className="text-gray-700 font-medium mb-2">
+                                Sessions Title: <span className='text-color2'>{course.sessionTitle}</span>
+                            </p>
+                            <p className="text-gray-700 font-medium mb-2">
+                                Drive Link: {course.driveLink}
+                            </p>
+                            <p className="text-gray-500 text-sm">
+                                Instructor: {course.tuitorUsername} (
+                                {course.tuitorEmail})
+                            </p>
+                            <div className="divider"></div>
+                            <div className='flex justify-between '>
                                 
-//                                 <button onClick={() => handelDelete(course._id)} className="bg-red-500  text-white px-3 py-1 rounded-sm">Delete</button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 ))}
+                                <button onClick={() => handelDelete(course._id)} className="bg-red-500  text-white px-3 py-1 rounded-sm">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
 
-//             </div>
-//         </div>
-//     );
-// };
+            </div>
+        </div>
+    );
+};
 
-// export default ViewAllMaterilas;
+export default ViewAllMaterilas;
